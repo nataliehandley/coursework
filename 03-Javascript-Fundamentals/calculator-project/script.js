cefa6516ec5b3948380cc71918d96ebce155e6a6
natalie-coder <63603124+natalie-coder@users.noreply.github.com>
2021-07-22 08:40:04 +0100

    added calculator project

---
+++ 03-Javascript-Fundamentals/calculator-project/script.js
@@ -0,0 +1,150 @@
+
+//Getting HTML element reference
+const additionEl = document.querySelector(".addition");
+const subtractionEl = document.querySelector(".subtraction");
+const multiplicationEl = document.querySelector(".multiplication");
+const divisionEl = document.querySelector(".division");
+const displayEl = document.querySelector(".display");
+const equalEl = document.querySelector('.equal');
+const buttonEl = document.querySelectorAll(".button");
+const operatorEl = document.querySelectorAll(".button-operator");
+
+buttonEl[11].value = 0;
+buttonEl[8].value = 1;
+buttonEl[9].value = 2;
+buttonEl[10].value = 3;
+buttonEl[5].value = 4;
+buttonEl[6].value = 5;
+buttonEl[7].value = 6;
+buttonEl[2].value = 7;
+buttonEl[3].value = 8;
+buttonEl[4].value = 9;
+
+operatorEl[4].value = "+";
+operatorEl[3].value = "-"
+operatorEl[2].value = "*";
+operatorEl[1].value = "/";
+operatorEl[5].value = "=";
+operatorEl[0].value = "ac"
+
+
+
+
+let buttonValue;
+var calculateResult =[];
+displayEl.innerHTML = 0;
+function numberClicked() {
+    for (var i = 0; i < buttonEl.length; i++) {
+    buttonEl[i].addEventListener("click", (event) => {
+        buttonValue = event.target.value;
+        console.log(buttonValue);
+        displayEl.innerHTML = buttonValue;
+        calculateResult.push(displayEl.innerHTML);
+        console.log(calculateResult);
+        //displayEl.value = buttonValue;
+      //return buttonValue;
+       
+})
+
+}
+}
+numberClicked();
+
+ let operatorValue;
+function operatorClicked() {
+    for (var i = 0; i < operatorEl.length; i++) {
+        operatorEl[i].addEventListener("click", (event) => {
+        operatorValue = event.target.value;
+        console.log(operatorValue);
+        displayEl.innerHTML = operatorValue;
+        calculateResult.push(displayEl.innerHTML);
+        console.log(calculateResult);
+        //return operatorValue;
+})
+}
+}
+ operatorClicked();
+ 
+
+let finalResult;
+function equalClicked() {
+        operatorEl[5].addEventListener("click", () => {
+          // let finalResult = parseInt(calculateResult[0]) + parseInt(calculateResult[2]);
+          // console.log(finalResult);
+          if (calculateResult[1] === "+") {
+            finalResult = parseInt(calculateResult[0]) + parseInt(calculateResult[2]);
+            console.log(finalResult);
+          } else if (calculateResult[1] === "-") {
+            finalResult = parseInt(calculateResult[0]) - parseInt(calculateResult[2]);
+            console.log(finalResult);
+          } else if (calculateResult[1] === "/") {
+            finalResult = parseInt(calculateResult[0]) / parseInt(calculateResult[2]);
+            console.log(finalResult);
+          } else if (calculateResult[1] === "*") {
+            finalResult = parseInt(calculateResult[0]) * parseInt(calculateResult[2]);
+            console.log(finalResult);
+          }
+          displayEl.innerHTML = finalResult;
+        })
+    }
+    equalClicked();
+
+    function clearDisplay() {
+      operatorEl[0].addEventListener("click", () => {
+        calculateResult = [];
+        displayEl.innerHTML = 0;
+//  var pendingVal = displayEl.innerHTML;
+//  var calculateResult =[];
+//  console.log(pendingVal);
+
+      })
+    }
+    clearDisplay();
+//  function performCalculation(event) {
+//      switch(operatorValue) {
+//          case "+":
+//              pendingVal = buttonValue;
+//              calculateResult.push(pendingVal);
+//              calculateResult.push("+");
+//              break;
+
+//      }
+//  }
+//  console.log(calculateResult);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // equalEl.addEventListener("click", () => {
+// //   displayEl.innerHTML="hello";
+// // })
+
+// // buttonEl[7].addEventListener("click", (event) => {
+// //   const firstInput = event.target.value;
+// //   displayEl.innerHTML = firstInput;
+// //  let calculatorArray = [firstInput];
+// //   console.log(calculatorArray);
+// // })
+
+
+// // buttonEl[9].addEventListener("click", (event) => {
+// //   const secondInput = event.target.value;
+// //   console.log(secondInput);
+// //   //displayEl.innerHTML =+ secondInput;
+// //   calculatorArray = [firstInput, secondInput];
+  
+// // })
---
+++ 03-Javascript-Fundamentals/calculator-project/script2.js
@@ -0,0 +1,48 @@
+
+
+// function calc () {
+//     var a = parseInt(document.querySelector("#value1").value); //convert string from input into a number as input type="text"
+//     var b = parseInt(document.querySelector("#value2").value);
+//     var op = document.querySelector("#operator").value;
+//     var calculate;
+
+//     if (op == "add") {
+//         calculate = a + b;
+//     } else if (op == "min") {
+//         calculate = a - b;
+//     } else if (op == "div") {
+//         calculate = a / b;
+//     } else if (op == "mul") {
+//         calculate = a * b;
+//     }
+//     document.getElementById("result").innerHTML = calculate;
+// }
+
+// let display = document.getElementById("display");
+
+// let buttons = Array.from(document.getElementsByClassName("button"));
+// //console.log(buttons);// gives a HTML collection of 20 buttons, needs to be converted to an array
+
+// buttons.map(button => {
+//     button.addEventListener("click", (event) => {
+//         // console.log("clicked"); //returns clicked
+//         // console.log(event); //returns MouseEvent
+//         // console.log(event.target); //div class="button"> 9 </div>
+//         // console.log(event.target.innerText); // returns 9
+//         // switch (event.target.innerText) {
+//         //     case "C":
+//         //         display.innerText = "";
+//         //         break;
+//         //     case "+":
+//         //         button[0] + button[1];
+
+//         //     default:
+//         //         display.innerText += event.target.innerText; //adding value of button to value of display
+//         if (event.target.innerText == "+") {
+            
+//         }
+//         }
+//     });
+// })
+
+
