31af34d33f8a5a979cda5acf04af0abe24fcf48d
natalie-coder <63603124+natalie-coder@users.noreply.github.com>
2021-08-01 12:42:15 +0100

    added functions to select random letter

--- 03-Javascript-Fundamentals/game-project/script.js
+++ 03-Javascript-Fundamentals/game-project/script.js
@@ -1,3 +1,5 @@
+//ASSIGN VALUES TO LETTER SQUARES
+
 const squareLetter = document.querySelectorAll(".square");
 
 squareLetter[0].value = "A";
@@ -27,7 +29,7 @@ squareLetter[23].value = "X";
 squareLetter[24].value = "Y";
 squareLetter[25].value = "Z";
 
-
+//TAKE INPUT FROM CLICK TO DETERMINE WHICH LETTER IS CLICKED
 let clickedLetter;
 function letterClicked () {
     for (var i = 0; i < squareLetter.length; i++) {
@@ -40,11 +42,54 @@ function letterClicked () {
 }
 letterClicked();
 
+//SEE IF LETTER CLICKED IS THE CORRECT LETTER
 function correctLetter () {
     if (clickedLetter === "A") {
-        console.log("Correct Answer!");
+        alert("Correct Answer!");
+
     } else {
-        console.log("Try again!");
+        alert("Try again!");
     }
 }
 correctLetter();
+
+//INSERT IMAGE USING JAVASCRIPT (GOOGLED THIS)
+var img = document.createElement("img");
+img.src = "./Images/A.jpeg";
+var src = document.getElementById("main-image");
+src.appendChild(img);
+
+
+//SELECTS RANDOM LETTER FROM ALPHABET (GOOGLED THIS)
+let selected;
+var imagesArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+function randomLetter() {
+    let selectedLetter = Math.floor(Math.random() * imagesArray.length);
+    selected = imagesArray[selectedLetter]
+    imagesArray = imagesArray.slice(0, selected).concat(imagesArray.slice(selected + 1));
+    console.log(selected);
+}
+
+randomLetter();
+
+//CONSOLE LOG RANDOM LETTER ON CLICK OF NEXT BUTTON
+var button = document.querySelector("#next-letter");
+button.addEventListener("click", () => {
+    randomLetter();
+    changeImage();
+
+})
+
+//SWITCH FUNCTION TO CHANGE IMAGE TO CORRESPOND WITH LETTER SELECTED
+function changeImage() {
+switch(selected) {
+    case "B":
+        img.src = "./Images/B.jpeg";
+        document.getElementById("h1-text").innerHTML = "BALL";
+        break;
+        default:
+            img.src = "./Images/A.jpeg";
+
+}
+}
+changeImage();
