cf0624d124c13196c4f0fe5731f2271584654f80
natalie-coder <63603124+natalie-coder@users.noreply.github.com>
2021-08-01 21:20:57 +0100

    added all images

--- 03-Javascript-Fundamentals/game-project/script.js
+++ 03-Javascript-Fundamentals/game-project/script.js
@@ -83,9 +83,109 @@ button.addEventListener("click", () => {
 //SWITCH FUNCTION TO CHANGE IMAGE TO CORRESPOND WITH LETTER SELECTED
 function changeImage() {
 switch(selected) {
+    case "A":
+        img.src = "./Images/A.jpeg";
+        document.getElementById("h1-text").innerHTML = "APPLE";
+        break;
     case "B":
         img.src = "./Images/B.jpeg";
         document.getElementById("h1-text").innerHTML = "BALL";
+        break;
+    case "C":
+        img.src = "./Images/C.jpeg";
+        document.getElementById("h1-text").innerHTML = "CAT";
+        break;
+    case "D":
+        img.src = "./Images/D.jpeg";
+        document.getElementById("h1-text").innerHTML = "DOG";
+        break;
+    case "E":
+        img.src = "./Images/E.jpeg";
+        document.getElementById("h1-text").innerHTML = "EGG";
+        break;
+    case "F":
+        img.src = "./Images/F.jpeg";
+        document.getElementById("h1-text").innerHTML = "FISH";
+        break;
+    case "G":
+        img.src = "./Images/G.jpeg";
+        document.getElementById("h1-text").innerHTML = "GOAT";
+        break;
+    case "H":
+        img.src = "./Images/H.jpeg";
+        document.getElementById("h1-text").innerHTML = "HAT";
+        break;
+    case "I":
+        img.src = "./Images/I.jpeg";
+        document.getElementById("h1-text").innerHTML = "IGLU";
+        break;
+    case "J":
+        img.src = "./Images/J.jpeg";
+        document.getElementById("h1-text").innerHTML = "JAM";
+        break;
+    case "K":
+        img.src = "./Images/K.jpeg";
+        document.getElementById("h1-text").innerHTML = "KING";
+        break;
+    case "L":
+        img.src = "./Images/L.jpeg";
+        document.getElementById("h1-text").innerHTML = "LION";
+        break;
+    case "M":
+        img.src = "./Images/M.jpeg";
+        document.getElementById("h1-text").innerHTML = "MOON";
+        break;
+    case "N":
+        img.src = "./Images/N.jpeg";
+        document.getElementById("h1-text").innerHTML = "NUT";
+        break;
+    case "O":
+        img.src = "./Images/O.jpeg";
+        document.getElementById("h1-text").innerHTML = "ORANGE";
+        break;
+    case "P":
+        img.src = "./Images/P.jpeg";
+        document.getElementById("h1-text").innerHTML = "PIG";
+        break;
+    case "Q":
+        img.src = "./Images/Q.jpeg";
+        document.getElementById("h1-text").innerHTML = "QUEEN";
+        break;
+    case "R":
+        img.src = "./Images/R.jpeg";
+        document.getElementById("h1-text").innerHTML = "BALL";
+        break;
+    case "S":
+        img.src = "./Images/S.jpeg";
+        document.getElementById("h1-text").innerHTML = "SUN";
+        break;
+    case "T":
+        img.src = "./Images/T.jpeg";
+        document.getElementById("h1-text").innerHTML = "TREE";
+        break;
+    case "U":
+        img.src = "./Images/U.jpeg";
+        document.getElementById("h1-text").innerHTML = "UMBRELLA";
+        break;
+    case "V":
+        img.src = "./Images/V.jpeg";
+        document.getElementById("h1-text").innerHTML = "VAN";
+        break;
+    case "W":
+        img.src = "./Images/W.jpeg";
+        document.getElementById("h1-text").innerHTML = "WATCH";
+        break;
+    case "X":
+        img.src = "./Images/X.jpeg";
+        document.getElementById("h1-text").innerHTML = "XRAY";
+        break;
+    case "Y":
+        img.src = "./Images/Y.jpeg";
+        document.getElementById("h1-text").innerHTML = "YOYO";
+        break;
+    case "Z":
+        img.src = "./Images/Z.jpeg";
+        document.getElementById("h1-text").innerHTML = "ZOO";
         break;
         default:
             img.src = "./Images/A.jpeg";
